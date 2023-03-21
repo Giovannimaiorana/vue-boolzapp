9a32f694c5a64e7850f4accd8901f52e1e94ca6e
Giovanni Maiorana <giovanni.maiorana.14@gmail.com>
2023-03-21 12:10:07 +0100

    viausalizzazione messaggio ed ora lista milestone5

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+const DateTime = luxon.DateTime;
 const { createApp } = Vue
 
   createApp({
@@ -6,6 +7,8 @@ const { createApp } = Vue
         contact:0,
         newMessage:"",
         testoRicerca:"",
+        orario: DateTime.now().setLocale('it').toLocaleString(DateTime.DATETIME_SHORT_WITH_SECONDS),
+        
         contacts: [
 {
 name: 'Michele',
@@ -13,17 +16,17 @@ avatar: './img/avatar_1.jpg',
 visible: true,
 messages: [
 {
-date: '10/01/2020 15:30:55',
+date: ' 15:30',
 message: 'Hai portato a spasso il cane?',
 status: 'sent'
 },
 {
-date: '10/01/2020 15:50:00',
+date: '15:50',
 message: 'Ricordati di stendere i panni',
 status: 'sent'
 },
 {
-date: '10/01/2020 16:15:22',
+date: ' 16:15',
 message: 'Tutto fatto!',
 status: 'received'
 }
@@ -37,17 +40,17 @@ avatar: "./img/avatar_2.jpg",
 visible: true,
 messages: [
 {
-date: '20/03/2020 16:30:00',
+date: '16:30',
 message: 'Ciao come stai?',
 status: 'sent'
 },
 {
-date: '20/03/2020 16:30:55',
+date: ' 16:30',
 message: 'Bene grazie! Stasera ci vediamo?',
 status: 'received'
 },
 {
-date: '20/03/2020 16:35:00',
+date: '16:35',
 message: 'Mi piacerebbe ma devo andare a fare la spesa.',
 status: 'sent'
 }
@@ -61,17 +64,17 @@ avatar: "./img/avatar_3.jpg",
 visible: true,
 messages: [
 {
-date: '28/03/2020 10:10:40',
+date: ' 10:10',
 message: 'La Marianna va in campagna',
 status: 'received'
 },
 {
-date: '28/03/2020 10:20:10',
+date: ' 10:20',
 message: 'Sicuro di non aver sbagliato chat?',
 status: 'sent'
 },
 {
-date: '28/03/2020 16:15:22',
+date: ' 16:15',
 message: 'Ah scusa!',
 status: 'received'
 }
@@ -85,12 +88,12 @@ avatar: "./img/avatar_4.jpg",
 visible: true,
 messages: [
 {
-date: '10/01/2020 15:30:55',
+date: ' 15:30',
 message: 'Lo sai che ha aperto una nuova pizzeria?',
 status: 'sent'
 },
 {
-date: '10/01/2020 15:50:00',
+date: ' 15:50',
 message: 'Si, ma preferirei andare al cinema',
 status: 'received'
 }
@@ -104,12 +107,12 @@ avatar: "./img/avatar_5.jpg",
 visible: true,
 messages: [
 {
-date: '10/01/2020 15:30:55',
+date: ' 15:30',
 message: 'Ricordati di chiamare la nonna',
 status: 'sent'
 },
 {
-date: '10/01/2020 15:50:00',
+date: '15:50',
 message: 'Va bene, stasera la sento',
 status: 'received'
 }
@@ -123,17 +126,17 @@ avatar: "./img/avatar_5.jpg",
 visible: true,
 messages: [
 {
-date: '10/01/2020 15:30:55',
+date: ' 15:30',
 message: 'Ciao Claudia, hai novità?',
 status: 'sent'
 },
 {
-date: '10/01/2020 15:50:00',
+date: ' 15:50',
 message: 'Non ancora',
 status: 'received'
 },
 {
-date: '10/01/2020 15:51:00',
+date: ' 15:51',
 message: 'Nessuna nuova, buona nuova',
 status: 'sent'
 }
@@ -147,12 +150,12 @@ avatar: "./img/avatar_7.jpg",
 visible: true,
 messages: [
 {
-date: '10/01/2020 15:30:55',
+date: ' 15:30',
 message: 'Fai gli auguri a Martina che è il suo compleanno!',
 status: 'sent'
 },
 {
-date: '10/01/2020 15:50:00',
+date: ' 15:50',
 message: 'Grazie per avermelo ricordato, le scrivo subito!',
 status: 'received'
 }
@@ -166,17 +169,17 @@ avatar: "./img/avatar_8.jpg",
 visible: true,
 messages: [
 {
-date: '10/01/2020 15:30:55',
+date: ' 15:30',
 message: 'Ciao, andiamo a mangiare la pizza stasera?',
 status: 'received'
 },
 {
-date: '10/01/2020 15:50:00',
+date: ' 15:50',
 message: 'No, l\'ho già mangiata ieri, ordiniamo sushi!',
 status: 'sent'
 },
 {
-date: '10/01/2020 15:51:00',
+date: ' 15:51',
 message: 'OK!!',
 status: 'received'
 }
@@ -190,14 +193,14 @@ status: 'received'
      },
      creazionemessaggio(){
       if(this.newMessage.length > 0){
-        this.contacts[this.contact].messages.push( {message:this.newMessage, status:'sent'});
+        this.contacts[this.contact].messages.push( { date:this.orario, message:this.newMessage, status:'sent'});
         this.newMessage="";
         this.risposta();
       }
      },
      risposta(){
       setTimeout(()=>{
-        this.contacts[this.contact].messages.push( {message:'ok ',status:'received'} );
+        this.contacts[this.contact].messages.push( { date:this.orario, message:'ok ',status:'received'} );
       },1000);
      },
      searchUser(){
@@ -206,10 +209,15 @@ status: 'received'
           element.visible=true;
         } else {
           element.visible=false;
-        }   
+        } 
       });
-
-     }
+     },
+        visualizzaMessaggio(contact){
+      return contact.messages[contact.messages.length -1].message;
+    },
+        visualizzaOrario(contact){
+          return contact.messages[contact.messages.length -1].date;
+        }
       
     }   
   }).mount('#app')
